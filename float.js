e472364e528a295b046667b84b6869d5319be542
phani <phani@brightsparks.in>
2015-08-01 10:24:08 +0530

    scroll with page

---
+++ float.js
@@ -0,0 +1,21 @@
+bloodAndTreasure.directive('float', [function () {
+  return {
+    restrict: 'C',
+    link: function(scope, element, attrs) {
+      var ele = angular.element(element);
+      $(window).scroll(function(){
+        if(bottomValue() > 1){
+          ele.css('bottom', bottomValue);
+        }
+        else{
+          ele.css('bottom', 0);
+          ele.addClass('round-borders')
+        }
+      });
+    }
+  };
+}]);
+
+function bottomValue(){
+  return $(document).height() - window.innerHeight - 222 - window.scrollY;
+}
