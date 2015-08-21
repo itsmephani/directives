9900f170e41d4e173734e890b17f039f94f862c0
Phani <itsmephani@users.noreply.github.com>
2015-08-21 19:59:27 +0530

    set placeholder if image not available

--- background_image.js
+++ background_image.js
@@ -1,13 +1,42 @@
-ang.directive('backgroundImage', [function () {
+wellthily.directive('backgroundImage', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'A',
+    scope: false,
     scope: {backgroundImage: '@'},
     link: function(scope, element, attrs) {
       var watcher = null;
+      var height = angular.element(element).hasClass('main-image') ? $rootScope.mainImageHeight : $rootScope.remainingImagesHeight;
+      angular.element(element).bind('onerror', function(){
+
+      });
+      function setImage(src) {
+        var img = new Image();
+
+        angular.element(element).css('height', height + 'px');
+
+        img.onload = function() {
+          element[0].style['background-image'] = 'url(' + src + ')';
+        };
+
+        img.onerror = function() {
+          if(attrs.placeHolder)
+            element[0].style['background-image'] = 'url(' + attrs.placeHolder + ')';
+          else
+            angular.element(element).css('height', height + 'px');
+        };
+
+        img.src = src; // fires off loading of image
+      }
       watcher = scope.$watch('backgroundImage', function(){
-        element[0].style['background-image'] = 'url(' + attrs.backgroundImage + ')';
-        element[0].style['background-size'] = '100% 100%';
-        if(attrs.backgroundImage)
+
+        if(scope.backgroundImage && (scope.backgroundImage != "" && scope.backgroundImage != '{}') ){
+          setImage(attrs.backgroundImage);
+        }else{
+          element[0].style['background-image'] = 'url('+ attrs.placeHolder +')';
+          angular.element(element).css('height', height + 'px');
+        }
+
+        if(scope.backgroundImage)
           watcher();
       });
     }
